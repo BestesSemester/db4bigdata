29ac8d396227e4b748048ef759f3e320d640d535
caos <marius@sys-tem.org>
2021-11-03 20:59:26 +0100

    readded fields

--- internal/model/db.go
+++ internal/model/db.go
@@ -1,10 +1,12 @@
 package model
 
+import "reflect"
+
 type Database interface {
-	Save(obj interface{})
-	Delete(obj interface{})
-	Find(qry string)
-	Close()
+	Save(obj interface{}) error
+	Delete(obj interface{}) error
+	Find(qry string, obj interface{}) error
+	Close() error
 }
 
 type StorageType int
@@ -36,4 +38,19 @@ func ConnectStorage(st StorageType) (Database, error) {
 	default:
 		return nil, nil
 	}
+}
+
+func resolveStructFields(inf interface{}) []reflect.StructField {
+	t := reflect.TypeOf(inf)
+	var strct reflect.Type
+	fields := []reflect.StructField{}
+	if t.Kind() == reflect.Ptr {
+		strct = t.Elem()
+	} else {
+		strct = t
+	}
+	for i := 0; i < strct.NumField(); i++ {
+		fields = append(fields, strct.Field(i))
+	}
+	return fields
 }
--- internal/model/mssql.go
+++ internal/model/mssql.go
@@ -1,13 +1,58 @@
 package model
 
-import "net/url"
+import (
+	"database/sql"
+	"log"
+	"net/url"
+	"reflect"
+
+	"github.com/sirupsen/logrus"
+)
 
 type MsSQLConfig struct {
 	Url      url.URL
 	UserName string
 	Password string
+	Database string
+}
+
+type MsSQL struct {
+	conn *sql.DB
 }
 
 func ConnectMsSQL(conf *MsSQLConfig) (Database, error) {
-	return nil, nil
+	mssql := &MsSQL{}
+	// conn, err := sql.Open("mssql", conf.Database)
+	mssql.conn = nil
+	return mssql, nil
+}
+
+// TODO: implement save logic
+func (mssql *MsSQL) Save(obj interface{}) error {
+	return nil
+}
+
+// TODO: implement delete logic
+func (mssql *MsSQL) Delete(obj interface{}) error {
+	return nil
+}
+
+// Returns sql-Result
+func (mssql *MsSQL) Find(qry string, target interface{}) error {
+	mssql.conn.Exec(qry)
+	t := reflect.TypeOf(target)
+	logrus.Println(t)
+	fields := resolveStructFields(target)
+	// result, err := nil
+	for _, field := range fields {
+		log.Println(field)
+	}
+	// logrus.Println(f.Tag.Get("mssql"))
+	return nil
+}
+
+// Closes the database connection (should only be used if you close it on purpose)
+func (mssql *MsSQL) Close() error {
+	err := mssql.conn.Close()
+	return err
 }
