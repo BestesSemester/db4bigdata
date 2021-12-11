98acdb8931dad230426e09c561020487db337ded
Kevin Sommer <kevin_sommer1993@gmx.de>
2021-12-11 13:12:26 +0100

    feat: uncomment everything

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	// "git.sys-tem.org/caos/db4bigdata/internal/importer"
 
-	"git.sys-tem.org/caos/db4bigdata/internal/model"
 	"git.sys-tem.org/caos/db4bigdata/internal/util"
 
 	"github.com/sirupsen/logrus"
@@ -16,25 +15,27 @@ func main() {
 	// mssql, _ := model.ConnectStorage(model.MSQL)
 	//
 	// p := model.Person{Name: "Scheffel"}
-	mongo, err := model.ConnectStorage(model.MongoDB)
-	if err != nil {
-		logrus.Fatal("Import to MongoDB failed: ", err)
-	}
-	var persons []model.Person
+
+	// ---------------------------------------------------------------------
+
+	// mongo, err := model.ConnectStorage(model.MongoDB)
+	// if err != nil {
+	// 	logrus.Fatal("Import to MongoDB failed: ", err)
+	// }
 	// Call importer
 	// **** Following lines just works in debug mode ****
 	// importer.ImportPersonsFromJSON("./../../generators/output_data/persons.json")
 	// importer.ImportInvoiceFromJSON("./../../generators/output_data/invoices.json")
 	// importer.ImportHierarchyFromJSON("./../../generators/output_data/hierarchy.json")
-	name := "Schott"
-	mongo.Find(name, &persons) //Actually just find by name
 
-	for s := range persons {
-		logrus.Info(s)
-	}
+	// Find object by name in mongo db
+	// var persons []model.Person
+	// name := "Schott"
+	// mongo.Find(name, &persons) //Actually just find by name
 
-	// logrus.Info(persons)
-	// mongo.Save()
+	// for s := range persons {
+	// 	logrus.Info(s)
+	// }
 
-	// mssql.Find("", &p)
+	// ---------------------------------------------------------------------
 }
