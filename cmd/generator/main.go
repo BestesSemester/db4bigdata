9db301df80fbd877f4e82ec92b1eb345e5259508
caos <sysexect@gmail.com>
2021-12-12 15:25:43 +0100

    added mssql generator

---
+++ cmd/generator/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	// "git.sys-tem.org/caos/db4bigdata/internal/importer"
+
+	"git.sys-tem.org/caos/db4bigdata/internal/importer"
+	"git.sys-tem.org/caos/db4bigdata/internal/model"
+	"git.sys-tem.org/caos/db4bigdata/internal/util"
+
+	"github.com/sirupsen/logrus"
+)
+
+// This package should run to generate and import new data into the database system
+func main() {
+	util.SetupLogs()
+	logrus.Println("hello")
+	mssql, err := model.ConnectStorage(model.MSQL)
+	if err != nil {
+		logrus.Fatalln(err)
+	}
+	//
+	// p := model.Person{Name: "Scheffel"}
+
+	// Call importer
+	// **** Following lines just works in debug mode ****
+	people := importer.ImportPersonsFromJSON("./generators/output_data/persons.json")
+	// importer.ImportInvoiceFromJSON("./generators/output_data/invoices.json")
+	// importer.ImportHierarchyFromJSON("./generators/output_data/hierarchy.json")
+	mssql.SavePersons(&people)
+	// mongo.Save()
+
+	// mssql.Find("", &p)
+}
